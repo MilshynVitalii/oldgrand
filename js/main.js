60bb659c4d936d44c66157f18797787a6f9108f4
MilshynVitalii <milshyn.vitalii@gmail.com>
2020-02-17 17:33:05 +0200

    Reviews section | adaptive | javascript is not full

--- js/main.js
+++ js/main.js
@@ -8,4 +8,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
       ? (menu.style.bottom = -menu.offsetHeight - 2 + "px")
       : (menu.style.bottom = "");
   });
+
+  let reviewsCategories = document.querySelector(".reviews__categories");
+  reviewsCategories.addEventListener("click", function(event) {
+    if (event.target.tagName != "A") return;
+    event.preventDefault();
+    let selected = this.querySelectorAll(".reviews__categories-link_active");
+    for (let elem of selected) {
+      elem.classList.remove("reviews__categories-link_active");
+    }
+    event.target.classList.add("reviews__categories-link_active");
+  });
+
+  let youtubeSlider = document.querySelector(".reviews__slider");
+  let youtubeSliderScroll = "0%";
+  youtubeSlider.addEventListener("click", function(event) {
+    if (event.target.tagName != "BUTTON") return;
+    if (event.target.classList.contains("reviews__slider-next")) {
+      youtubeSliderScroll =
+        parseFloat(youtubeSliderScroll) > 90
+          ? "99.99%"
+          : parseFloat(youtubeSliderScroll) + 33.33 + "%";
+      this.firstElementChild.style.transform = `translateX(-${youtubeSliderScroll})`;
+    }
+    if (event.target.classList.contains("reviews__slider-prev")) {
+      youtubeSliderScroll =
+        parseFloat(youtubeSliderScroll) <= 0
+          ? "0%"
+          : parseFloat(youtubeSliderScroll) - 33.33 + "%";
+      this.firstElementChild.style.transform = `translateX(-${youtubeSliderScroll})`;
+    }
+  });
 });
